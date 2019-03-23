a761eb0b653b770c29c73400ed7e0e22a7eb89d0
Luca D'Amato <luca.damato@edu.unito.it>
2019-03-23 22:27:04 +0100

    correctly removed stopwords

--- preprocessing.py
+++ preprocessing.py
@@ -1,68 +1,59 @@
 #!/usr/bin/env python3
 
-"""
+'''
 preprocessing.py: a script which removes stopwords from
                     the root texts and saves the results
                     in another file.
-"""
+'''
 
 import nltk
+import re
 import pandas as pd
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from functools import reduce
 
-""" helper functions """
-
-
-def compose(*functions):
-    return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
-
-
-def drop_void(xs):
-    return xs.map(lambda x: (x, None)[x == []]).dropna()
+''' helper functions '''
 
 
 def drop_stopwords(xs):
-    return xs.map(
-        lambda x: (x, None)[None in map(
-            lambda y: (y, None)[y in stop_words], x
-        )]
-    ).dropna()
+    return list(filter(lambda y: y not in stop_words, xs))
 
 
-def stringlist_to_tokenlist(str):
-    return word_tokenize(str.replace('[', '')
-                            .replace(']', '')
-                            .replace("'", '')
-                            .replace(',', ''))
+def string_to_list(str):
+    return re.sub(r'^(\[\')|(\',\s\')|(\'])$', '|', str).split('|')[1:-1]
+
 
 lowercased_path = "./dataset/reviews_filtered_20_5_lowercased.csv"
 nostopwords_path = "./dataset/reviews_filtered_20_5_lowercased_nostopwords.csv"
 
 stop_words = set(stopwords.words('english'))
-data_frame = pd.read_csv('./dataset/reviews_filtered_20_5.csv', sep=";")
+data_frame = pd.read_csv(lowercased_path, sep=";")
 
-""" 1: bring everything to lowercase """
+''' 1: bring everything to lowercase '''
 
 data_frame['text'] = data_frame['text'].map(lambda x: x.lower())
 data_frame['root_text'] = data_frame['root_text'].map(lambda x: x.lower())
 
 data_frame.to_csv(lowercased_path, index=False, sep=";")
 
-""" 2: remove rows with stopwords """
+''' 2: remove rows with stopwords '''
 
-csv_text = data_frame['text']
-csv_root_text = data_frame['root_text']
+data_frame['root_text'] = data_frame['root_text'].map(
+    lambda x: string_to_list(x)
+)
 
-csv_root_text = csv_root_text.map(lambda x: stringlist_to_tokenlist(x))
-csv_text = csv_text.map(lambda x: stringlist_to_tokenlist(x))
+data_frame['text'] = data_frame['text'].map(
+    lambda x: string_to_list(x)
+)
 
-clear = compose(drop_stopwords, drop_void)
+data_frame['text'] = pd.Series(
+    map(drop_stopwords, data_frame['text'])
+)
 
-data_frame['text'] = clear(csv_text)
-data_frame['root_text'] = clear(csv_root_text)
-data_frame.dropna()
+data_frame['root_text'] = pd.Series(
+    map(drop_stopwords, data_frame['root_text'])
+)
 
 data_frame.to_csv(nostopwords_path, index=False, sep=";")
