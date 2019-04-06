28c25a19a6257f283e42e583e494c958dcb89556
Luca D'Amato <luca.damato@edu.unito.it>
2019-04-06 13:18:59 +0200

    refactored medium_polarity to a more general form, defined c3

--- utility.py
+++ utility.py
@@ -37,18 +37,17 @@ def item_reviews(i):  # rev_item
     return utility[utility['listing_id'] == i]
 
 
-def medium_polarity(pol_set):  # mp_*
-    # pol_set is rev_*['polarity']
-    return pol_set.sum()/pol_set.count()
+def medium(set):
+    return set.sum() / set.count()
 
 
 def c1(r):
-    mp_user_r = medium_polarity(user_reviews(user(r))['polarity'])
+    mp_user_r = medium(user_reviews(user(r))['polarity'])
     review_pol_disp = abs(r['polarity'] - mp_user_r)
     max_disp = max(
         utility.apply(
             lambda row: abs(
-                row['polarity'] - medium_polarity(
+                row['polarity'] - medium(
                     user_reviews(row['reviewer_id'])['polarity'])
             ), axis=1))
 
@@ -56,12 +55,26 @@ def c1(r):
 
 
 def c2(r):
-    mp_item_r = medium_polarity(item_reviews(item(r))['polarity'])
+    mp_item_r = medium(item_reviews(item(r))['polarity'])
     review_pol_disp = abs(r['polarity'] - mp_item_r)
     max_disp = max(
         utility.apply(
             lambda row: abs(
-                row['polarity'] - medium_polarity(
+                row['polarity'] - medium(
                     item_reviews(row['listing_id'])['polarity'])
             ), axis=1))
+
     return review_pol_disp / max_disp
+
+
+def c3(r):
+    mlw_user_r = medium(user_reviews(user(r))['review_length'])
+    review_len_disp = abs(r['review_length'] - mlw_user_r)
+    max_disp = max(
+        utility.apply(
+            lambda row: abs(
+                row['review_length'] - medium(
+                    user_reviews(row['reviewer_id'])['review_length'])
+            ), axis=1))
+
+    return review_len_disp / max_disp
