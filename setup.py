c51c853ef89e9a73a0156712c2d332e9556001d9
Luca D'Amato <luca.damato@edu.unito.it>
2019-10-10 22:11:01 +0200

    added setup and README

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="RvQuality",
+    version="1.0",
+    description="A python library that establishes the quality of a review. Based on Luca D'Amato's thesis.",
+    author="Luca D'Amato",
+    author_email="luca.damato@edu.unito.it",
+    url="https://gitlab.com/Eskilop/rvquality",
+    packages=['rvquality', 'rvquality.components']
+)
